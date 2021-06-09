32cc513e05efc546eb8456846c7f907a7a6e6502
funduleadiana <diana.fundulea@yahoo.com>
2021-06-09 10:16:58 +0100

    introducing navLink for client side routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
@@ -34,15 +34,28 @@ const NotFoundPage=()=>(
         404 Error Page not found! <Link to="/">Go Home</Link>
     </div>
 )
+
+const Header =()=>(
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create expenses</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
+)
 const routes = (
     <BrowserRouter>
-    <Switch>
-    <Route path="/" component={ExpenseDashboardPage} exact/>
-    <Route path="/create" component={AddExpensePage} />
-    <Route path="/edit" component={EditPage} />
-    <Route path="/help" component={HelpPage} />
-    <Route component={NotFoundPage}/>
-    </Switch>
+        <div>
+            <Header/>
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact/>
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditPage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage}/>
+            </Switch>
+        </div>
     </BrowserRouter>
 );
 
