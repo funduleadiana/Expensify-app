1a78734a932acdc8757ae983deb462a630fd53da
funduleadiana <diana.fundulea@yahoo.com>
2021-06-11 11:26:09 +0100

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,10 @@ import { NavLink } from 'react-router-dom';
 
 const Header =()=>(
     <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create expenses</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        <h1>Portfolio App</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+        <NavLink to="/portfolio" activeClassName="is-active">Portfolio</NavLink>
+        <NavLink to="/contact" activeClassName="is-active">Contact</NavLink>
     </header>
 );
 
