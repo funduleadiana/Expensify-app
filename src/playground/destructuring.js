dfda8b4e36c5ac978703d2807f9d346b4aa8af33
funduleadiana <diana.fundulea@yahoo.com>
2021-06-16 17:29:32 +0100

    destructuring arrays

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,20 +1,47 @@
-const person = {
+// const person = {
 
-    age: 26, 
-    location: {
-        city: 'London',
-        temp: 26
-    }
-};
+//     age: 26, 
+//     location: {
+//         city: 'London',
+//         temp: 26
+//     }
+// };
 
-const { name: firstName = 'Dylan', age } = person;
+// const { name: firstName = 'Dylan', age } = person;
 
-// instead of writing person.age person.name
-console.log(`${firstName} is ${age}.`);
+// // instead of writing person.age person.name
+// console.log(`${firstName} is ${age}.`);
 
-const {city, temp: temperature}= person.location;
-if(city && temperature){
-    console.log(`It's ${temperature} is ${city}.`)
-}
+// const {city, temp: temperature}= person.location;
+// if(city && temperature){
+//     console.log(`It's ${temperature} is ${city}.`)
+// }
 
-// person.location.temp 
+// // person.location.temp 
+
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// }
+
+// const {name: publisherName = 'Self-Published'} = book.publisher;
+// console.log(publisherName )
+
+
+const address = ['22 Elm way', 'London', 'England', 'w79ha']
+const [street, city, country, postcode] = address;
+
+console.log(`You are in ${city}, ${country}`);
+
+// const [, , myCountry = 'Germany'] = address
+console.log(`My address is in ${country}`)
+
+
+const item = ['Coffee (hot)', '$2.00', '$2.5', '$2.72'];
+
+const [beverage, , medium] = item
+
+console.log(`A medium ${beverage} costs ${medium} `)
