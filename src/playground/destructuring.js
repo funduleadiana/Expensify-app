399fc55f932ae2e37c3178ca2b1cf02698d08e2c
funduleadiana <diana.fundulea@yahoo.com>
2021-06-16 17:09:58 +0100

    default values

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,5 +1,5 @@
 const person = {
-    name: 'Andrew',
+
     age: 26, 
     location: {
         city: 'London',
@@ -7,14 +7,14 @@ const person = {
     }
 };
 
-const { name, age } = person;
+const { name: firstName = 'Dylan', age } = person;
 
 // instead of writing person.age person.name
-console.log(`${name} is ${age}.`);
+console.log(`${firstName} is ${age}.`);
 
-const {city, temp}= person.location;
-if(city && temp){
-    console.log(`It's ${temp} is ${city}.`)
+const {city, temp: temperature}= person.location;
+if(city && temperature){
+    console.log(`It's ${temperature} is ${city}.`)
 }
 
 // person.location.temp 
