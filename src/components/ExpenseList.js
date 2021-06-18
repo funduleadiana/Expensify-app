690f81193ac3a526d5ad39dde6e423dab5b4b0fb
funduleadiana <diana.fundulea@yahoo.com>
2021-06-18 10:07:46 +0100

    rendering an individual expense

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { connect} from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expensesSelector';
 
 
 const ExpenseList = (props)=> (
     <div>
         <h1>Expense List</h1>
-        {props.expenses.length}
+        {props.expenses.map((expense)=> {
+            return <ExpenseListItem key={expense.id} {...expense}/>
+        } )}
     </div>
 );
 
 const mapStateToProps = (state)=> {
     return {
-        expenses: state.expenses,
-        filters: state.filters
+        expenses: selectExpenses(state.expenses, state.filters)
     }
 }
 
 
 
 export default connect(mapStateToProps)(ExpenseList); //HOC
-// Higher order component - a component (HOC) that renders another component
-// Reuse code
-// Render hijacking
-// Prop manipulation
-// Abstract state
