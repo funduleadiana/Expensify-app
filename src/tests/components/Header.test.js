54abbea4357377617787ea185f6ef0239acb1539
funduleadiana <diana.fundulea@yahoo.com>
2021-06-25 16:12:23 +0100

    configure serializer to automatically read toJSON(wrapper)

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,7 +5,7 @@ import toJSON from 'enzyme-to-json';
 
 test('should render Header correctly', ()=> {
     const wrapper = shallow(<Header/>);
-    expect(toJSON(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
     //expect(wrapper.find('h1').text()).toBe('Expensify')
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header/>);
