bdadaa86787e21b5229034ad476c47f958924962
funduleadiana <diana.fundulea@yahoo.com>
2021-06-29 18:09:07 +0100

    turning Edit page into a class component

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -5,12 +5,12 @@ import expenses from '../fixtures/expensesArray';
 
 
 
- let onSubmit, history, wrapper;
+ let addExpense, history, wrapper;
 
 beforeEach(()=>{
-    onSubmit = jest.fn();
+    addExpense = jest.fn();
     history = {push: jest.fn()};
-    wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>)
+    wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history}/>)
 })
 
  test('should render AddExpensePage correctly', ()=>{
@@ -19,8 +19,8 @@ beforeEach(()=>{
  });
 
  test('should handle onSubmit', ()=>{
-     
+
      wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
      expect(history.push).toHaveBeenLastCalledWith('/');
-     expect(onSubmit).toHaveBeenLastCalledWith(expenses[1])
+     expect(addExpense).toHaveBeenLastCalledWith(expenses[1])
  })
